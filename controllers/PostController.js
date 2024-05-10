aff022353f5e5c51f0ad4ed455f2ea2136e23c6e
Eva Garcia <evagarciaclemente96@gmail.com>
2024-05-10 13:18:21 +0200

    endpoint create

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,16 @@
 const Post = require("../models/Post");
 
 const PostController ={
-    
+    async create(req,res){
+      try {
+        const post = await Post.create(req.body)
+        res.status(201).send(post)
+      } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+      }
+    }
 }
+
+
+module.exports = PostController
