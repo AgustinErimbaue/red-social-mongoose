35847a89aa9affdb9233b48dde510c8277e3d0d8
Eva Garcia <evagarciaclemente96@gmail.com>
2024-05-11 13:41:34 +0200

    ruta getById

--- routes/posts.js
+++ routes/posts.js
@@ -6,6 +6,7 @@ const router = express.Router()
 router.post("/",PostController.create)
 router.put("/id/:_id",PostController.update)
 router.get("/", PostController.getAll)
+router.get("/id/:_id", PostController.getById)
 
 
 module.exports = router
