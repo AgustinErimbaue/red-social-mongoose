bc2e23eea80e887506fdf3e500b6e6db88182cf9
Eva Garcia <evagarciaclemente96@gmail.com>
2024-05-11 14:10:03 +0200

    ruta eliminar por ID

--- routes/posts.js
+++ routes/posts.js
@@ -7,6 +7,7 @@ router.post("/",PostController.create)
 router.put("/id/:_id",PostController.update)
 router.get("/", PostController.getAll)
 router.get("/id/:_id", PostController.getById)
+router.delete("/id/:_id", PostController.deleteById)
 
 
 module.exports = router
