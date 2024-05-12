568a4062d32ab38e3f6c788c171a9da9867b7267
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-05-12 20:16:58 +0200

    correccion de bug

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -3,7 +3,7 @@ const Comment = require("../models/Comment");
 const CommentController = {
   async create(req, res) {
     try {
-      const comment = await Comment.create(req, res);
+      const comment = await Comment.create(req.body);
       res.status(201).send(comment);
     } catch (error) {
       console.error(error);
