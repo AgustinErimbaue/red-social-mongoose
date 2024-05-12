f3f8c026ddda05f21c4f769ee0037b1436bb3e03
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-05-12 19:34:27 +0200

    Agregar ruta de comment, correcion en el modelo de comment, agregar app.use de comments en index

--- models/Comment.js
+++ models/Comment.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const CommentSchema = new mongoose.Schema(
   {
     username: String,
-    Body: string,
+    Body: String,
   },
   { timestamps: true }
 );
 
 const Comment = mongoose.model("Comment", CommentSchema);
 
-module.exports = Comment
+module.exports = Comment;
