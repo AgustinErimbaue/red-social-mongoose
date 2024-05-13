5f47d2dc0cb70dc59bf2d3bab6c7b5311380a03d
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-05-13 12:59:27 +0200

    se instala bcryptjs, se modifica controlador register y se agrega el encriptado de la password

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,13 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/keys");
 
 const UserController = {
   async register(req, res) {
     try {
-      const user = await User.create(req.body);
+      const password = bcrypt.hashSync(req.body.password, 10);
+      const user = await User.create({...req.body,password});
       res.status(201).send({ message: "Usuario creado con exito", user });
     } catch (error) {
       console.error(error);
