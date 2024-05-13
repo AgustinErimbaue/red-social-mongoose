d04a8c6ecfd84746d6445ff6a626ddc3a005b145
Eva Garcia <evagarciaclemente96@gmail.com>
2024-05-13 14:07:35 +0200

    paginación

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,9 +1,11 @@
 const Comment = require("../models/Comment");
+const Post = require("../models/Post");
 
 const CommentController = {
   async create(req, res) {
     try {
       const comment = await Comment.create(req.body);
+      // Post.findByIdAndUpdate
       res.status(201).send(comment);
     } catch (error) {
       console.error(error);
--- models/Comment.js
+++ models/Comment.js
@@ -1,13 +1,16 @@
-const mongoose = require("mongoose");
+const {default:mongoose, Types }= require("mongoose")
+const ObjectId = mongoose.SchemaTypes.ObjectId
 
 const CommentSchema = new mongoose.Schema(
   {
     username: String,
     body: String,
+    userId: Types.ObjectId,
+    postId: Types.ObjectId
+
   },
   { timestamps: true }
-);
+)
+const Comment = mongoose.model("Comment", CommentSchema)
 
-const Comment = mongoose.model("Comment", CommentSchema);
-
-module.exports = Comment;
+module.exports = Comment
