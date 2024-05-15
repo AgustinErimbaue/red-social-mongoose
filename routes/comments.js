2cf55f818ef9db0d7d711dee5f1cf8720acea140
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-05-15 11:22:33 +0200

    correcion bug

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const CommentController = require("../controllers/CommentController");
 const { authentication, isAuthor } = require("../middleware/authentication");
-const { authentication } = require("../middleware/authentication");
 const router = express.Router();
 
 router.post("/id/:_id",authentication, authentication, CommentController.create);
