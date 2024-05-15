a99bde0814b4883deaf91685f7d4c8c9dd1dccb9
Eva Garcia <evagarciaclemente96@gmail.com>
2024-05-15 14:06:08 +0200

    prueba

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 const PostController = {
   async create(req, res) {
     try {
-      const post = await Post.create({ ...req.body, userId: req.user._id });
+      const post = await Post.create({ ...req.body, author: req.user._id });
       await User.findByIdAndUpdate(req.user._id, {
         $push: { postIds: post._id },
       });
@@ -14,41 +14,9 @@ const PostController = {
       res.status(500).send(error);
     }
   },
-  async update(req, res) {
-    try {
-      const post = await Post.findByIdAndUpdate(req.params._id, req.body, {
-        new: true,
-      });
-      res.send({ msg: "Post actualizado correctamente", post });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send(error);
-    }
-  },
-  async getAll(req, res) {
-    try {
-      const { page = 1, limit = 10 } = req.query;
-      const posts = await Post.find()
-        .populate({
-          path: "commentIds",
-          populate: {
-            path: "userIds",
-          },
-        })
-<<<<<<< HEAD
-        .limit(parseInt(limit))
-        .skip((page - 1) * parseInt(limit));
-=======
-        await User.findByIdAndUpdate(req.user._id, {$push: {postIds:post._id}})
-        res.status(201).send(post)
-      } catch (error) {
-        console.log(error)
-        res.status(500).send(error)
-      }
-    },
     async update(req,res){
       try {
-        const post = await Post.findByIdAndUpdate(req.params._id, {...req.body,author:req.author}, {new:true})
+        const post = await Post.findByIdAndUpdate(req.params._id, {...req.body,author:req.user._id}, {new:true})
         res.send({msg:"Post actualizado correctamente", post})
       } catch (error) {
         console.log(error)
@@ -83,7 +51,7 @@ const PostController = {
     },
     async deleteById(req,res){
       try {
-        const post = await Post.findByIdAndDelete(req.params._id, {...req.body,author:req.author})
+        const post = await Post.findByIdAndDelete(req.params._id, {...req.body,author:req.user._id})
         res.send({msg:"Publicación borrada exitosamente", post})
       } catch (error) {
         console.log(error)
@@ -102,45 +70,6 @@ const PostController = {
         console.log(error)
       }
     }  
-}
->>>>>>> eva
-
-      res.send(posts);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send(error);
-    }
-  },
-  async getById(req, res) {
-    try {
-      const post = await Post.findById(req.params._id);
-      res.send(post);
-    } catch (error) {
-      console.log(error);
-      res.status(500).send(error);
-    }
-  },
-  async deleteById(req, res) {
-    try {
-      const post = await Post.findByIdAndDelete(req.params._id);
-      res.send({ msg: "Publicación borrada exitosamente", post });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send(error);
-    }
-  },
-  async getPostByUserName(req, res) {
-    try {
-      if (req.params.username.length > 20) {
-        return res.status(400).send("Búsqueda demasiado larga");
-      }
-      const userName = new RegExp(req.params.username, "i");
-      const posts = await Post.find({ userName });
-      res.send(posts);
-    } catch (error) {
-      console.log(error);
-    }
-  },
 };
 
 module.exports = PostController;
